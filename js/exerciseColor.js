74e25b219906bf50375e11c97143760b441c8203
James Volmert <james.volmert@gmail.com>
2016-03-31 13:44:02 -0600

    worked with controller

--- js/exerciseColor.js
+++ js/exerciseColor.js
@@ -2,4 +2,12 @@ var app = angular.module('exercise', []);
 
 app.controller('exerciseController', function($scope) {
     $scope.name = "Baby Blue";
+    $scope.secondsInCentury = 60 * 60 * 24 * 365 * 100;
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1;
+    var yyyy = today.getFullYear();
+
+    $scope.rightNow = mm + '/' + dd + '/' + yyyy;
+
 });
