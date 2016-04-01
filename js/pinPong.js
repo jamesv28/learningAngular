6698d54fca37ab106850553e7c273fbd341d88b5
James Volmert <james.volmert@gmail.com>
2016-04-01 11:43:18 -0600

    wored on friday stuff

--- js/pinPong.js
+++ js/pinPong.js
@@ -1,3 +1,17 @@
-/**
- * Created by jamesvolmert on 4/1/16.
- */
+var app = angular.module('myApp', []);
+
+app.controller('myController', function($scope) {
+
+    $scope.score = 0;
+    $scope.score2 = 0;
+    $scope.addScore = function() {
+        $scope.score++;
+    };
+    $scope.addScore2 = function() {
+        $scope.score2++;
+    };
+
+    //var colorSwitch = function() {
+    //    if($scope.score >)
+    //}
+});
